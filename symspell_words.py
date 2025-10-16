c0001468f54e7cbb8b701f2cd926138150d955b6
Dimitrios Athanasopoulos <jimathanas0@yahoo.gr>
2025-10-16 16:15:38 +0300

    Update symspell_words.py

--- symspell_words.py
+++ symspell_words.py
@@ -16,12 +16,32 @@ with open('/home/jimmys/glossapi/spellcheck/symspell_wrong_words.csv', 'w', enco
         
         words = set()
         for word in text.split():
-            #Normalization epeidi exoun diaforetiko encoding oi lekseis kai tis bgazei san lathos
+            # Skip numbers, emails, links
+             # if 
+            # 1. Normalize NFKC
             word_norm = unicodedata.normalize("NFKC", word)
-            if bool(re.search(r'[α-ωΑ-Ωά-ώ]', word_norm)):
+            # 2. Lowercase
+            word_norm = word_norm.lower()
+            # 3. Remove punctuation
+            word_norm = ''.join(c for c in word_norm if not unicodedata.category(c).startswith('P'))
+            # 4. Check if the word is Greek
+            if bool(re.search(r'[α-ωΑ-Ω]', word_norm)):
+                # Strip all accent marks
+                word_norm = ''.join(c for c in word_norm if unicodedata.category(c) != 'Mn')
+                # Check if the word is in the dictionary
                 if word_norm not in sym_spell.words:
+                    # Check if the string is 2>= words
+                    word_segments = sym_spell.word_segmentation(word_norm)
+                    if len(word_segments.words) > 1:
+                        words.add(word_norm)
+                else:
                     words.add(word_norm)
-        
+            # 5. Its English
+            else:
+                pass
+                    
+
+            
         if words:
             f.write(f"{filename}\n")
             for word in sorted(words):
